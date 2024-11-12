f330e277668ee79979eb432bf3a5361490b19833
msnkr <mikyle.designs@gmail.com>
2024-11-12 10:20:00 +0200

    .

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -6,6 +6,10 @@ import { useState } from "react";
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
 
+  function handleClick() {
+    setNavbar((prev) => !prev);
+  }
+
   return (
     <div className="p-16 flex justify-between items-center">
       <div>
@@ -16,12 +20,26 @@ export default function Navbar() {
         <div className="mode-section">
           <img src={dark} className="w-[30px] cursor-pointer" />
         </div>
-        <div className="relative">
+        <div>
           <img
             src={burger}
             className="w-[40px] cursor-pointer"
             alt="burger-icon"
+            onClick={handleClick}
           />
+        </div>
+        <div className="relative">
+          <ul
+            className={`${
+              navbar
+                ? "opacity-100 translate-x-0 visible"
+                : "opacity-0 -translate-x-1/2 invisible"
+            } absolute text-end right-8 top-8 duration-100 space-y-4 pl-40 p-2 text-xl font-semibold`}
+          >
+            <li className="cursor-pointer">Projects</li>
+            <li className="cursor-pointer w-40">About Me</li>
+            <li className="cursor-pointer">Contact</li>
+          </ul>
         </div>
       </div>
     </div>
