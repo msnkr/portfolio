974d9d6ad759ba611b241c981dc2c854b991c2aa
msnkr <mikyle.designs@gmail.com>
2024-11-12 07:37:03 +0200

    .

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -20,7 +20,9 @@ export default function App() {
           </p>
         </div>
       </div>
-      <div></div>
+      <div>
+        <Projects />
+      </div>
     </div>
   );
 }
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,7 +1,10 @@
+import projectOne from "../images/project-1.png";
+import projectTwo from "../images/project-2.png";
+import projectThree from "../images/project-3.png";
+import projectFour from "../images/project-4.png";
+import projectFive from "../images/project-5.png";
+import projectSix from "../images/project-6.png";
+
 export default function Projects() {
-  return (
-    <div>
-      <></>
-    </div>
-  );
+  return <div></div>;
 }
