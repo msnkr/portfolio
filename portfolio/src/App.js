d0f6835750d0ef8f31f64b63bc814bae5c26df3e
msnkr <mikyle.designs@gmail.com>
2024-11-13 11:33:16 +0200

    .

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,6 +6,7 @@ import machineLearning from "./data/machineLearning";
 import Skills from "./components/Skills";
 
 import { useState } from "react";
+import { useInView } from "react-intersection-observer";
 const skillArr = [
   "HTML",
   "CSS",
@@ -25,6 +26,14 @@ const skillArr = [
   "Illustrator",
 ];
 export default function App() {
+  const { ref: mainRef, inView: mainInView } = useInView({
+    threshold: 0.4,
+  });
+
+  const { ref: aboutRef, inView: aboutInView } = useInView({
+    threshold: 0.4,
+  });
+
   return (
     <div className="px-16 py-8 font-custom md:px-32 lg:px-52 xl:px-80">
       <div className="navbar-section py-16">
@@ -32,17 +41,26 @@ export default function App() {
       </div>
       <div className="main-container mt-20 tracking-tighter">
         <div>
-          <p>
-            <span className="text-3xl tracking-wider lg:text-4xl">Hi.</span>
-            <br />{" "}
-            <span className="text-4xl font-bold lg:text-5xl tracking-wide">
-              I'm Mikyle Kruger
-            </span>
-          </p>
-          <p className="mt-8 lg:w-1/2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <div
+            ref={mainRef}
+            className={`${
+              mainInView
+                ? "translate-x-0 visible opacity-100"
+                : "-translate-x-1/2 invisible opacity-0"
+            } wording duration-500`}
+          >
+            <p>
+              <span className="text-3xl tracking-wider lg:text-4xl">Hi.</span>
+              <br />{" "}
+              <span className="text-4xl font-bold lg:text-5xl tracking-wider lg:w-1/2">
+                I'm Mikyle Kruger
+              </span>
+            </p>
+            <p className="mt-8 lg:w-1/2">
+              A digital printer and graphic designer with a love for programming
+              and web-development
+            </p>
+          </div>
           <div className="py-16">
             <Grid arr={projects} />
           </div>
@@ -53,7 +71,7 @@ export default function App() {
           Skills
         </p>
         <div className="mt-8">
-          <ul className="grid grid-cols-2 gap-2 lg:grid-cols-3">
+          <ul className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
             {skillArr.map((skill, index) => (
               <Skills skill={skill} />
             ))}
@@ -61,22 +79,36 @@ export default function App() {
         </div>
       </div>
       <div className="about-me-container">
-        <div>
-          <p className="text-3xl font-semibold lg:text-4xl tracking-wide">
-            About Me
+        <div
+          ref={aboutRef}
+          className={`${
+            aboutInView
+              ? "translate-x-0 visible opacity-100"
+              : "-translate-x-1/2 invisible opacity-0"
+          } wording duration-500`}
+        >
+          <div>
+            <p className="text-3xl font-semibold lg:text-4xl tracking-wide">
+              About Me
+            </p>
+          </div>
+          <p className="mt-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
-        <p className="mt-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
         <div className="py-16">
           <Grid arr={machineLearning} />
+          <p className="px-4 underline underline-offset-4 text-sm text-gray-500 cursor-pointer">
+            <a href="https://github.com/msnkr/kaggle_comps" target="_blank">
+              View more...
+            </a>
+          </p>
         </div>
       </div>
       <div className="footer">
--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,3 +1,7 @@
 export default function Skills({ skill }) {
-  return <li className="font-semibold cursor-pointer">{skill}</li>;
+  return (
+    <li className="font-semibold cursor-pointer hover:animate-bounce">
+      {skill}
+    </li>
+  );
 }
