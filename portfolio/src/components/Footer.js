9c18364f1e096fbc8272297cadd8a276c94ce4ca
msnkr <mikyle.designs@gmail.com>
2024-11-25 09:13:45 +0200

    .

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,16 +4,19 @@ import logo from "../images/GitHub_Logo.png";
 
 export default function Footer() {
   return (
-    <div>
+    <div className="flex justify-evenly items-center">
       <div className="text-center my-16">
         <p className="text-5xl font-bold tracking-wide">msnkr</p>
+      </div>
+      <div>
+        <img src={logo} className="w-40" />
       </div>
       <div>
         <div className="relative">
           <Link to="navbar" smooth={true} duration={800}>
             <img
               src={arrowIcon}
-              className="w-12 animate-bounce cursor-pointer absolute bottom-0 right-0"
+              className="w-12 animate-bounce cursor-pointer"
             />
           </Link>
         </div>
